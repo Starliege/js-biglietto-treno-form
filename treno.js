8c0b781fd2b4a7884713d0dd55ae9cbcf300988a
Starliege <urrianif@gmail.com>
2022-07-21 15:52:47 +0200

    prima milestone fatta

---
+++ treno.js
@@ -0,0 +1,27 @@
+const btn= document.getElementById("btn");
+
+btn.addEventListener('click', function(){
+  let dist = document.getElementById("dist").value;
+  let age = document.getElementById("age").value;
+
+  let price = dist     * 0.21;
+
+  if (age <= 0) {
+      alert("Noooneeeeee");
+      location.reload();
+    }
+    else if (age < 18) {
+      price = price - ((price / 100) * 20);
+    }
+    else if (age >= 65 && age < 140) {
+      price = price - ((price / 100) * 40);
+    }
+    else if (age > 140) {
+      alert("Seh vabbè");
+      alert("Ripartiamo da 0");
+      location.reload();
+    }
+    price = price.toFixed(2);
+    document.getElementById("prezzo-biglietto").innerHTML = price;
+    console.log("Il prezzo del tuo biglietto è:", price)
+});
